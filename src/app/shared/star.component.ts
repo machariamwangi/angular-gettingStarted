eb692aac4f2efe1cc4b06d002034a354921a7597
Elimax Mwangi <elimax@localhost.localdomain>
2019-11-05 12:04:18 -0500

    passing data to and from nested components

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, OnChanges, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
-    selector: 'pm-star',
-    templateUrl: './star.component.html',
-    styleUrls: ['./star.component.css']
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnChanges{
-rating: number = 4;
-startWidth: number;
-ngOnChanges(): void{
-    this.startWidth = this.rating * 75/ 5;
-}
+export class StarComponent implements OnChanges {
+  @Input() rating: number;
+  starWidth: number;
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
+
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 75 / 5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+  }
 }
